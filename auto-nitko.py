17c636ae6119f1358efbee6008dd1acdd069ff33
WillyJ <wjlorenzo@gmail.com>
2022-09-29 12:30:47 -0400

    Setup argparse/help and IP validation

--- auto-nitko.py
+++ auto-nitko.py
@@ -6,9 +6,19 @@
 # Description: Find open HTTP/s Port and check them with Nitko
 
 import sys
-import socket
+import argparse
+import ipaddress
 
-if len(sys.argv) == 2:
-    scantarget = socket.gethostbyname(sys.argv[1])
-else:
-    print("Syntax: auto-nitko.py <ip>")
+# Setup our help
+usage="%s -i IPv4 Address" % sys.argv[0]
+parser=argparse.ArgumentParser()
+parser.add_argument("-i","--ip-address", help="IPv4 Address")
+args = parser.parse_args()
+
+# Make sure this is really an IP address
+try:
+    ip = ipaddress.ip_address(sys.argv[2])
+except ValueError:
+    print('%s is an invalid IP Address' % sys.argv[2])
+except:
+    print('Syntax: %s -i <ip>' % sys.argv[0])
